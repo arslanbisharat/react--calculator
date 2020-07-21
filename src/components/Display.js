c695b1d5d83257736515f6f01b7d86a32ac3b8bd
arslanbisharat <thearslan380@gmail.com>
2020-07-21 14:03:27 -0400

    miletsone 5 code

---
+++ src/components/Display.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends React.Component {
+  render() {
+    const { value, operation } = this.props;
+
+    return (
+      <div className="display">
+        <div className="display-symbol">
+          {operation}
+        </div>
+        <div className="display-value">
+          {parseFloat(value)}
+        </div>
+      </div>
+    );
+  }
+}
+
+Display.propTypes = {
+  value: PropTypes.string,
+  operation: PropTypes.string,
+};
+
+Display.defaultProps = {
+  value: '0',
+  operation: null,
+};
+
+export default Display;
